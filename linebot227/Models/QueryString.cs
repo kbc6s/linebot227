ebce85d9a477e975855e482925aa8adbd78597f8
oxy F <f129546497@gmail.com>
2019-03-10 20:23:08 +0800

    add query string

---
+++ linebot227/Models/QueryString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace linebot227.Models
+{
+    public class QueryString
+    {
+    }
+}
--- linebot227/default.aspx.cs
+++ linebot227/default.aspx.cs
@@ -13,15 +13,23 @@ namespace linebot227
         const string channelAccessToken = "fTBvt+oi30MpuWqTvT/KJDBuKDJ8iKxPhJLX5fHwT+bha1vEfZPfprFFQ7LrdgdyrDnx/yDe1C+hTbLtYxojWGRyAbRVz2iuok8WbUiZBeOn3gxlUjs5gpsGQmmySmmF9m/Uat9ZwLWxomFA3FZ6jgdB04t89/1O/w1cDnyilFU=";
         const string AdminUserId= "U8168367ec76c449dbdd98410d9333b8b";
         const string SendTo = "U8168367ec76c449dbdd98410d9333b8b";
+       // public object Label2;
+        public string Label2 { get; private set; }
+        public string Label1 { get; private set; }
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Label2 = Request.QueryString["name"];
         }
-
         protected void Button1_Click(object sender, EventArgs e)
         {
             var bot = new Bot(channelAccessToken);
-            bot.PushMessage(AdminUserId, $"測試 {DateTime.Now.ToString()} ! ");
+            //bot.PushMessage(AdminUserId, $"測試 {DateTime.Now.ToString()} ! ");
+            bot.PushMessage(AdminUserId, Label2);
+            string s_url;
+            s_url = "?name=" + Label2;
+            Response.Redirect(s_url);
         }
 
         protected void Button2_Click(object sender, EventArgs e)
